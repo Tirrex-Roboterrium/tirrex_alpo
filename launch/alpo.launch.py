88cdef930136b511e5736ef51c46fe07e8b2e60d
jean.laneurit <jean.laneurit@inrae.fr>
2023-03-19 16:21:43 +0100

    update configuration

--- launch/alpo.launch.py
+++ launch/alpo.launch.py
@@ -1,9 +1,18 @@
+from tirrex_demo import (
+    get_log_directory,
+    get_debug_directory,
+    get_demo_timestamp,
+    get_demo_config_directory,
+    save_replay_configuration,
+)
 from launch import LaunchDescription
 
 from launch.actions import (
     IncludeLaunchDescription,
     DeclareLaunchArgument,
     OpaqueFunction,
+    GroupAction,
+    SetEnvironmentVariable,
 )
 
 from launch.substitutions import LaunchConfiguration
@@ -13,25 +22,50 @@ from ament_index_python.packages import get_package_share_directory
 
 def launch_setup(context, *args, **kwargs):
 
+    robot_namespace = "alpo"
+    demo = "tirrex_alpo"
+    demo_timestamp = get_demo_timestamp()
     mode = LaunchConfiguration("mode").perform(context)
     record = LaunchConfiguration("record").perform(context)
+    demo_config_directory = LaunchConfiguration("demo_config_directory").perform(context)
+    debug_directory = get_debug_directory(demo, demo_timestamp, record)
+    log_directory = get_log_directory(demo, demo_timestamp, record)
 
-    configuration_directory = LaunchConfiguration("configuration_directory").perform(
-        context
+    print(" demo_config_directory ", demo_config_directory)
+    print(" debug_directory ", debug_directory)
+    print(" log_directory ", log_directory)
+
+    actions = []
+
+    # in rolling : use launch_ros/launch_ros/actions/set_ros_log_dir.py instead
+    actions.append(SetEnvironmentVariable("ROS_LOG_DIR", log_directory))
+
+    actions.append(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                get_package_share_directory("tirrex_demo")
+                + "/launch/demo.launch.py"
+            ),
+            launch_arguments={
+                "demo": demo,
+                "demo_timestamp": demo_timestamp,
+                "mode": mode,
+                "record": record,
+                "robot_namespace": robot_namespace,
+            }.items(),
+        )
     )
 
-    robot = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            get_package_share_directory("tirrex_demo") + "/launch/demo.launch.py"
-        ),
-        launch_arguments={
-            "mode": mode,
-            "configuration_directory": configuration_directory,
-            "record" : record,
-        }.items(),
-    )
+    if record == "true":
 
-    return [robot]
+        save_replay_configuration(
+            demo,
+            demo_timestamp,
+            "alpo.launch.py",
+            {"mode": "replay_"+mode},
+        )
+
+    return [GroupAction(actions)]
 
 
 def generate_launch_description():
@@ -42,7 +76,7 @@ def generate_launch_description():
 
     declared_arguments.append(
         DeclareLaunchArgument(
-            "configuration_directory",
+            "demo_config_directory",
             default_value=get_package_share_directory("tirrex_alpo") + "/config",
         )
     )
